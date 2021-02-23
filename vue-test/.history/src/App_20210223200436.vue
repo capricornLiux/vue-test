c6428d7a935230b1cf775ebbbe114392fca05806
liuxin <xin.liu@medbanks.cn>
2021-02-23 20:23:31 +0800

    临时提交

---
+++ vue-test/.history/src/App_20210223200436.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <column-list :list="data"></column-list>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'xxx',
+    description: 'sdf',
+    avatar: 'sdfs'
+  },
+  {
+    id: 1,
+    title: 'xxx',
+    description: 'sdf',
+    avatar: 'sdfs'
+  },
+  {
+    id: 1,
+    title: 'xxx',
+    description: 'sdf',
+    avatar: 'sdfs'
+  },
+  {
+    id: 1,
+    title: 'xxx',
+    description: 'sdf',
+    avatar: 'sdfs'
+  }
+]
+export default defineComponent({
+  name: 'App',
+  components: {
+    ColumnList
+  },
+  setup () {
+    return {
+      data: testData
+    }
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-test/.history/src/components/ColumnList_20210223194520.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <li v-for="column in list" :key="column.id">
+      <img :src="column.avatar" alt="">
+      <h5>{{column.title}}</h5>
+      <p>{{column.description}}</p>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+export interface ColumnProps {
+  id: number;
+  title: string;
+  avatar: string;
+  description: string;
+}
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      required: true
+    }
+  }
+})
+</script>
